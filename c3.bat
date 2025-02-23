b5167bd801567d0ca384d87bed863789cd012668
Zhangtongoscar <13701033228@139.com>
2025-02-23 23:16:45 +0800

    refactor: 重构项目并添加批处理脚本注释
    - 移除了 c3_create_s2t_conn.cpp 文件
    - 更新了 c3.bat 和 c4.bat 批处理脚本的注释
    - 添加了新的项目结构和注释，为后续的 Python 脚本开发做准备

--- c3.bat
+++ c3.bat
@@ -1,4 +1,5 @@
 @echo off
+@REM 从csv导入点和边 到neo4j
 pushd %~dp0
 echo Running c3_create_s2t_conn.py...
 python c3_create_s2t_conn.py
--- c4.bat
+++ c4.bat
@@ -1,4 +1,5 @@
 @echo off
+@REM 基于在mysql数据库表，导出仿真设备及点到neo4j
 pushd %~dp0
 echo Running c4_import_mysql_sim.py...
 python c4_import_mysql_sim.py
