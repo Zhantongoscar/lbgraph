f49d4f2716267d48f45b4ec37c4644526cb077c6
Zhangtongoscar <13701033228@139.com>
2025-02-27 00:49:10 +0800

    feat(c1b): 新增设备数据导入功能
    - 新增 c1b_create_vertexpoint_table.cpp 文件，实现设备数据导入功能
    - 修改 c1b.bat 文件，使用新的设备数据导入程序
    - 更新 build_log.txt 文件，记录新的编译过程

--- c1b.bat
+++ c1b.bat
@@ -1,17 +1,3 @@
-@REM @echo off
-@REM chcp 65001
-@REM echo 正在编译...
-@REM g++ c1_create_devicetype_table.cpp -o devicetype.exe -fexec-charset=utf-8
-@REM if %errorlevel% equ 0 (
-@REM     echo 编译成功！正在运行程序...
-@REM     devicetype.exe
-@REM     echo.
-@REM     echo 程序运行完成！
-@REM ) else (
-@REM     echo 编译失败！
-@REM )
-@REM pause
-
 @echo off
 chcp 65001
 echo 正在编译...
@@ -22,7 +8,7 @@ set MYSQL_LIB="C:/clib/mysql/lib"
 echo Include path: %MYSQL_INC%
 echo Library path: %MYSQL_LIB%
 
-g++ -v c1_create_devicetype_table.cpp -o devicetype.exe -I%MYSQL_INC% -L%MYSQL_LIB% -g -Wall "C:/clib/mysql/lib/libmysql.lib" -lwsock32 -lws2_32 > build_log.txt 2>&1
+g++ -v c1b_create_vertexpoint_table.cpp -o c1b_vertexpoint.exe -I%MYSQL_INC% -L%MYSQL_LIB% -g -Wall "C:/clib/mysql/lib/libmysql.lib" -lwsock32 -lws2_32 > build_log.txt 2>&1
 
 if %ERRORLEVEL% NEQ 0 (
     echo Compilation failed. See build_log.txt for details.
@@ -48,5 +34,5 @@ echo 编译成功！
 echo 复制MySQL库文件...
 copy "C:\clib\mysql\lib\libmysql.dll" . /Y
 echo 正在运行程序...
-.\devicetype.exe
+.\c1b_vertexpoint.exe
 pause
